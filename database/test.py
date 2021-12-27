1f73bdd340a9cf21118c387c0c347c5f339fd332
Tien Duong <tien.lyf.d@gmail.com>
2021-12-26 23:36:54 -0800

    updated redshift create table sql

--- database/test.py
+++
@@ -1,8 +0,0 @@
-from create_table_sql import drop_table_queries, create_table_queries
-
-for i in drop_table_queries:
-    print(f"\n{i}")
-    
-for e in create_table_queries:
-    print(f"\n{e}")
-    
